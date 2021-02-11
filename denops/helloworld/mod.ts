55cd90c323100ea8bf7668dc9d5a96afc9cb408e
Alisue <lambdalisue@hashnote.net>
2021-02-12 02:01:22 +0900

    Update example

--- denops/helloworld/mod.ts
+++ denops/helloworld/mod.ts
@@ -1,4 +1,4 @@
-import { Denops } from "../../../denops-deno/denops.ts";
+import { Denops } from "https://deno.land/x/denops@v0.1/denops.ts";
 
 // Create denops server
 const denops = new Denops({
@@ -6,13 +6,23 @@ const denops = new Denops({
     return Promise.resolve(`${text}`);
   },
 
-  async say(name: unknown): Promise<void> {
-    if (typeof name !== "string") {
-      throw new Error("'name' must be a string");
+  async say(app: unknown): Promise<void> {
+    if (typeof app !== "string") {
+      throw new Error("'app' must be a string");
     }
-    await denops.command(`echomsg 'Hello Denops ${name}'`);
+    const name = await denops.call("input", "Your name: ");
+    const version = await denops.eval("v:version");
+    await denops.command("redraw");
+    await denops.command(
+      `echomsg 'Hello ${name}. Your are using ${app} in Vim/Neovim ${version}'`
+    );
   },
 });
+
+// Add command
+await denops.command(
+  'command! DenopsHelloWorld call denops#request("helloworld", "say", ["Denops"])'
+);
 
 await denops.debug("denops-helloworld debug message");
 await denops.info("denops-helloworld info message");
