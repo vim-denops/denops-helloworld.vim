9c18975afaf9df0280889ffa95d5fa954bddded0
Alisue <lambdalisue@hashnote.net>
2021-02-12 00:32:24 +0900

    Update

--- denops/helloworld/mod.ts
+++ denops/helloworld/mod.ts
@@ -2,17 +2,21 @@ import { Denops } from "../../../denops-deno/denops.ts";
 
 // Create denops server
 const denops = new Denops({
-  echo: async (text: unknown): Promise<unknown> => {
+  async echo(text: unknown): Promise<unknown> {
     return Promise.resolve(`${text}`);
   },
 
-  say: async (name: unknown): Promise<void> => {
+  async say(name: unknown): Promise<void> {
     if (typeof name !== "string") {
       throw new Error("'name' must be a string");
     }
-    await denops.command(`echo "Hello Denops ${name}"`);
+    await denops.command(`echomsg 'Hello Denops ${name}'`);
   },
 });
+
+await denops.debug("denops-helloworld debug message");
+await denops.info("denops-helloworld info message");
+await denops.error("denops-helloworld error message");
 
 // Wait until the server is closed
 await denops.waitClosed();
