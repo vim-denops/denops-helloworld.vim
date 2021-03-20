1ee5ef67177c5a334fdd6b42f127ba4d4a2b4257
Alisue <lambdalisue@hashnote.net>
2021-03-20 12:51:00 +0900

    Follow the latest API changes
    https://github.com/vim-denops/denops-std-deno/pull/6

--- denops/helloworld/mod.ts
+++ denops/helloworld/mod.ts
@@ -1,5 +1,5 @@
 // Import 'start' function from denops_std
-import { start } from "https://deno.land/x/denops_std@v0.3/mod.ts";
+import { start } from "https://deno.land/x/denops_std@v0.4/mod.ts";
 
 // Call 'start' with async callback. The callback get 'vim' instance.
 start(async (vim) => {
@@ -59,8 +59,6 @@ start(async (vim) => {
       console.log("t:denops_helloworld", await vim.t.get("denops_helloworld"));
       // Use 'vim.v.get' to access Vim's variable
       console.log("v:errmsg", await vim.v.get("errmsg"));
-      // Use 'vim.env.get' to access environment variable
-      console.log("$DENOPS_HELLOWORLD", await vim.env.get("DENOPS_HELLOWORLD"));
     },
 
     async set_variables(): Promise<void> {
@@ -74,8 +72,6 @@ start(async (vim) => {
       await vim.t.set("denops_helloworld", "Tabpage HOGEHOGE");
       // Use 'vim.v.set' to replace Vim's variable
       await vim.v.set("errmsg", "Vim HOGEHOGE");
-      // Use 'vim.env.set' to replace environment variable
-      await vim.env.set("DENOPS_HELLOWORLD", "Env HOGEHOGE");
     },
 
     async remove_variables(): Promise<void> {
@@ -89,8 +85,6 @@ start(async (vim) => {
       await vim.t.remove("denops_helloworld");
       // Use 'vim.v.remove' to remove Vim variable
       await vim.v.remove("errmsg");
-      // Use 'vim.env.remove' to remove environment variable
-      await vim.env.remove("DENOPS_HELLOWORLD");
     },
 
     async register_autocmd(): Promise<void> {
@@ -122,13 +116,6 @@ start(async (vim) => {
     command! DenopsSetVariables echo denops#notify("${vim.name}", "set_variables", [])
     command! DenopsRemoveVariables echo denops#notify("${vim.name}", "remove_variables", [])
     command! DenopsRegisterAutocmd echo denops#notify("${vim.name}", "register_autocmd", [])
-
-    let g:denops_helloworld = "Global Hello Denops"
-    let b:denops_helloworld = "Buffer Hello Denops"
-    let w:denops_helloworld = "Window Hello Denops"
-    let t:denops_helloworld = "Tabpage Hello Denops"
-    let v:errmsg = "Vim Hello Denops"
-    let $DENOPS_HELLOWORLD = "Env Hello Denops"
   `);
 
   console.log("denops-helloworld.vim (std) has loaded");
