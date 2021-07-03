c3d990c510aca865ae3b8013d0c80ac6acc94673
Alisue <lambdalisue@hashnote.net>
2021-07-03 23:30:18 +0900

    Update denops_std to v1.0.0-alpha.3

--- denops/helloworld/vendor/https/deno.land/x/denops_std/helper/mod.ts
+++ denops/helloworld/vendor/https/deno.land/x/denops_std/helper/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/denops_std@v1.0.0-alpha.0/helper/mod.ts";
+export * from "https://deno.land/x/denops_std@v1.0.0-alpha.3/helper/mod.ts";
--- denops/helloworld/vendor/https/deno.land/x/denops_std/variable/mod.ts
+++ denops/helloworld/vendor/https/deno.land/x/denops_std/variable/mod.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/denops_std@v1.0.0-alpha.0/variable/mod.ts";
+export * from "https://deno.land/x/denops_std@v1.0.0-alpha.3/variable/mod.ts";
